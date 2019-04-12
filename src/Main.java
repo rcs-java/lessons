ba87defc19621fb3070501a81e4c0c5964995019
Toms Kusiņš <t.kusins@gmail.com>
2019-04-12 18:50:42 +0300

    Arrays extra homework

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+        Advanced, optional extra task:
+- application outputs the count of each letter (ignore case)
+Note: given the limitations, this can be done using only arrays (no need for ArrayLists or HashMaps)
+         */
+
         Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        word = word.toLowerCase();
+        int letterCounts[] = new int[26];
 
-        int[] userInput = new int[5];
-
-        for (int i = 0; i < 5; i++) {
-            userInput[i] = sc.nextInt();
+        for (int i = 0; i < word.length(); i++) {
+            char letter = word.charAt(i);
+            int index = letter - 97;
+            letterCounts[index]++;
         }
 
-        for (int inp : userInput) {
-            System.out.println(inp);
+        for (int i = 0; i < 26; i++) {
+            char letter = (char) (i + 97);
+            System.out.println(letter + ": " + letterCounts[i]);
         }
     }
 }
