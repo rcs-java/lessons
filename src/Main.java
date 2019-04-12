6aea79f61463770a3fb39f066f03801f75ee38c3
Toms Kusiņš <t.kusins@gmail.com>
2019-04-12 20:24:50 +0300

    Exceptions example

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String word = sc.next();
-        word = word.toLowerCase();
-        int letterCounts[] = new int[26];
-
-        for (int i = 0; i < word.length(); i++) {
-            char letter = word.charAt(i);
-            int index = letter - 97;
-            letterCounts[index]++;
+        int userInput = 0;
+        try {
+            userInput = getUserInput();
+        } catch (InputMismatchException whatever) {
+            System.out.println(whatever.getMessage());
+            return;
         }
 
-        for (int i = 0; i < 26; i++) {
-            char letter = (char) (i + 97);
-            System.out.println(letter + ": " + letterCounts[i]);
+        userInput++;
+        System.out.println("Your input plus one is: " + userInput);
+    }
+
+    private static int getUserInput() throws InputMismatchException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input a number from 1 to 10");
+
+        int i = 0;
+        try {
+            i = sc.nextInt();
+        } catch (InputMismatchException failRead) {
+            throw new InputMismatchException("Please input a number!");
+        }
+
+        if (i < 1) {
+            throw new InputMismatchException("Number too small");
+        } else if (i > 10) {
+            throw new InputMismatchException("Number too large");
+        } else {
+            return i;
         }
     }
 }
